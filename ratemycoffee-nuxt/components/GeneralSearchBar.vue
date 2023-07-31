0ce4bee360096f883fce84f0edb9bf3301d296bb
smsanagustin <smsanagustin@up.edu.ph>
2023-07-31 23:48:00 +0800

    fixed scaling

--- ratemycoffee-nuxt/components/GeneralSearchBar.vue
+++ ratemycoffee-nuxt/components/GeneralSearchBar.vue
@@ -3,13 +3,13 @@
         <div class="w-1/2">
             <div class="text-lg flex pr-5 rounded-xl search-box border-2 border-black">
                 <div class="px-4 search-field w-full flex align-center">                
-                    <i class="mt-4">
+                    <i class="mt-3">
                         <font-awesome-icon :icon="['fas', 'search']" />
                     </i>
                     <input class="focus:outline-none ml-3 w-11/12" type="text" placeholder="Search by coffee, coffee shop or location...">
                 </div>
                 <div class="dropdown">
-                    <div class="default-option">{{option}}</div>
+                    <p class="default-option"><span class="">{{ option }}</span></p>
                     <ul v-if="showCategory">
                         <li @click="switchOption">Search by coffee</li>
                         <li @click="switchOption">Search by coffee shop</li>
@@ -59,6 +59,9 @@
     border-left: 2px solid black;
 }
 
+.default-option {
+}
+
 .search-box .dropdown ul {
     width: 242px;
     position: absolute;
@@ -67,11 +70,12 @@
     padding-left: 26px;
     background-color: white;
     border: black solid 2px;
-    padding-top: 19px;
+    /* padding-top: 19px; */
     /* display: none; */
 }
 
 .search-box .dropdown ul li {
+    margin-top: 20px;
     margin-bottom: 20px;
     cursor: pointer;
 }
